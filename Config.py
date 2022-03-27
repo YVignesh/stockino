665db583e9bf804341ef44f329b0d60d1d86649c
YVignesh <yvigneshkumar61@gmail.com>
2022-03-27 23:11:43 +0530

    27-03-2022

--- Config.py
+++ Config.py
@@ -1,7 +1,7 @@
 Config = {
     "SECRET_KEY" : "wzojc!zm8r8)0a=km17vvhac8#b3tq1=f9x=13rq10ryx0$cgd",
-    "EMAIL_USER" : "",
-    "EMAIL_PASS" : "",
+    "EMAIL_USER" : "teespring612@gmail.com",
+    "EMAIL_PASS" : "vzwniawpbrzntkes",
     "DEBUG_VALUE" : "True",
     "START_TIME" : "2022/03/16:00:00:00",
     "STOP_TIME" : "2022/04/01:00:00:00"
